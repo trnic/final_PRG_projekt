dd0d0018b78044349b05671287e9565f467b9cd3
trnec <trnkalukas@student.voskh.cz>
2026-05-12 11:33:34 +0200

    verze2

---
+++ data.js
@@ -0,0 +1,6 @@
+export const rawTrainings = [
+    { type: 'sprint', duration: 30, intensity: 9, distance: 400 },
+    { type: 'endurance', duration: 60, intensity: 6, averageHeartRate: 145 },
+    { type: 'shooting', duration: 45, intensity: 5, shots: 50, accuracy: 0.8 },
+    { type: 'sprint', duration: 20, intensity: 10, distance: 200 }
+];
---
+++ main.js
@@ -0,0 +1,85 @@
+import { rawTrainings } from './data.js';
+class Training {
+    _duration;
+    _intensity;
+    constructor(duration, intensity) {
+        // Validace dat v konstruktoru
+        if (duration <= 0)
+            throw new Error("Délka trvání musí být kladná.");
+        if (intensity < 1 || intensity > 10)
+            throw new Error("Intenzita musí být mezi 1 a 10.");
+        this._duration = duration;
+        this._intensity = intensity;
+    }
+    /** Společná metoda pro výpis informací */
+    getSummary() {
+        return `Trénink: trvání ${this._duration} min, intenzita ${this._intensity}/10`;
+    }
+}
+class SprintTraining extends Training {
+    distance;
+    constructor(duration, intensity, distance) {
+        super(duration, intensity);
+        if (distance <= 0)
+            throw new Error("Vzdálenost musí být kladná.");
+        this.distance = distance;
+    }
+    calculateCalories() {
+        // Primitivní výpočet pro sprint
+        return this._duration * this._intensity * 1.5;
+    }
+    calculateLoad() {
+        return (this.distance / this._duration) * this._intensity;
+    }
+}
+class EnduranceTraining extends Training {
+    averageHeartRate;
+    constructor(duration, intensity, avgHeartRate) {
+        super(duration, intensity);
+        if (avgHeartRate <= 0)
+            throw new Error("Tepová frekvence musí být kladná.");
+        this.averageHeartRate = avgHeartRate;
+    }
+    calculateCalories() {
+        // Primitivní výpočet pro vytrvalost
+        return this._duration * (this.averageHeartRate / 10);
+    }
+    calculateLoad() {
+        return this._duration * this._intensity;
+    }
+}
+class ShootingTraining extends Training {
+    shots;
+    accuracy;
+    constructor(duration, intensity, shots, accuracy) {
+        super(duration, intensity);
+        this.shots = shots;
+        this.accuracy = accuracy;
+    }
+    calculateCalories() {
+        return this._duration * 5; // Střelba pálí méně kalorií
+    }
+    calculateLoad() {
+        return this.shots * this.accuracy;
+    }
+}
+const trainingInstances = rawTrainings.map(data => {
+    switch (data.type) {
+        case 'sprint':
+            return new SprintTraining(data.duration, data.intensity, data.distance || 0);
+        case 'endurance':
+            return new EnduranceTraining(data.duration, data.intensity, data.averageHeartRate || 0);
+        case 'shooting':
+            return new ShootingTraining(data.duration, data.intensity, data.shots || 0, data.accuracy || 0);
+        default:
+            throw new Error("Neznámý typ tréninku");
+    }
+});
+console.log("--- ANALÝZA FOTBALOVÝCH TRÉNINKŮ ---");
+trainingInstances.forEach((training, index) => {
+    console.log(`Záznam č. ${index + 1}:`);
+    console.log(training.getSummary());
+    console.log(`Vypočtené kalorie: ${training.calculateCalories().toFixed(0)} kcal`);
+    console.log(`Vypočtené zatížení: ${training.calculateLoad().toFixed(1)} j.`);
+    console.log("------------------------------------");
+});
