db7bce5fed1a59f6fa28e12e907ac3026b3e2cd5
trnec <187640649+trnic@users.noreply.github.com>
2026-06-08 20:54:52 +0200

    upresneni_html

--- data.js
+++ data.js
@@ -2,5 +2,5 @@ export const rawTrainings = [
     { type: 'sprint', duration: 30, intensity: 9, distance: 400 },
     { type: 'endurance', duration: 60, intensity: 6, averageHeartRate: 145 },
     { type: 'shooting', duration: 45, intensity: 5, shots: 50, accuracy: 0.8 },
-    { type: 'sprint', duration: 20, intensity: 10, distance: 200 }
+    { type: 'skills', duration: 20, intensity: 10, accuracy: 200 }
 ];
